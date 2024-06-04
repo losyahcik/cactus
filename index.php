67063c6254433380a62554f09f8bc0ea608f2a27
losyahcik <egor.zhilin29@gmail.com>
2024-06-04 15:37:05 +0400

    добавлены мета теги

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <meta name="Keywords" content="кактусы, купить кактусы, магазин кактусов, кактус, кактус онлайн, смотреть кактусы, алоэ, цереус, купить алоэ.">
+    <meta name="description" content="Интернет магазин кактусов">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="favicon.ico">
